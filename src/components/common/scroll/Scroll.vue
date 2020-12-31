07293924a6078c8eb2e8075896c7f21ad7d8a9c4
dty999 <dingtongya@aliyun.com>
2020-12-31 20:13:57 +0800

    dev

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="scroll-wapper" ref='scroll'>
+    <div class="scroll-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    name: 'Scroll',
+    data() {
+      return {
+        scroll: null
+      }
+    },
+    mounted() {
+      this.scroll = new BScroll(this.$refs.scroll, {
+        observeDOM: true,
+        click: true
+      })
+      // this.scroll.refresh()
+      // console.log(this.scroll);
+    }
+  }
+</script>
+
+<style>
+</style>
